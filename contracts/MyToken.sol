67e802dd5192e38e9b666ebe10cbe32f8631b82f
YangYoungWoo <yyw1000@naver.com>
2025-04-13 22:42:03 +0900

    added approve and transferFrom

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.28;
 
 contract MyToken {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed spender, unit256 amount)
+
+
     string public name;
     string public symbol;
     uint8 public decimals; // uint8 --> 8 bit unsigned int
@@ -12,6 +16,7 @@ contract MyToken {
     
     // 누가 얼마를 가지고 있느냐
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) allowance;
 
 
     constructor(string memory _name, string memory _symbol, uint8 _decimal, uint256 _amount) {
@@ -24,11 +29,30 @@ contract MyToken {
         _mint(_amount*10**uint256(decimals), msg.sender); //1 MT
     }
 
+    function approve(address spender, uint256 amount) external {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(spender, amount);
+    }
+
+    //tranferFrom 은 허락받은 사람이 호출함 -> owner를 파라미터로 받아야함이 transfer와의 차이점
+    function transferFrom(address from, address to, uint256 amount) external {
+        address spender = msg.sender;
+        require(allowance[from][spender] >= amount, "insufficient allowance");
+        allowance[from][spender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+
+        emit Transfer(from, to, amount);
+
+    }
+
 
 
     function _mint(uint256 amount, address owner) internal {
         totalSupply += amount;
         balanceOf[owner] += amount;
+
+        emit Transfer(address(0), owner, amount);
     }
     
     function transfer(uint256 amount, address to) external {
@@ -36,6 +60,8 @@ contract MyToken {
         require(balanceOf[msg.sender] >= amount, "insufficient balance");
         balanceOf[msg.sender] -= amount;
         balanceOf[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
     }
 
     //function totalSupply() external view returns (uint256) {
@@ -49,6 +75,22 @@ contract MyToken {
     //function name() external view returns (string memory) {
     //    return name;
     //}
+
+
+
+
+    /*
+    approve
+    - allow spender address to send my token
+    transferFOrm
+    - spender :owner -> target address
+
+    * token owner --> bank contract
+    * token owner --> router contract --> bank contract
+    * token owner --> router contract --> bank contract(multi contract)
+
+
+    */
 
 
 }
